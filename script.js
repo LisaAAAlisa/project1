42c36c7ec8062b663dbec33f4c94cffda8d3f559
LisaAAAlisa <alisaamacedo@gmail.com>
2020-11-17 18:17:39 -0600

    adding some html formatting for input

--- script.js
+++ script.js
@@ -21,6 +21,7 @@
     }).then(function (from) {
         console.log(from)
         $("#convertedFrom").text(from.value);
+        $("#convertedTo").text(from.value);
     searchCurrencyURL(from);
     //  var from = $("#convertedFrom").text(response);
     // var to = $("#convertedTo").text(respons);
@@ -34,9 +35,10 @@
             //this object[property] is the long form name of our currency
             //inside this for loop we are going to create a new option html element
     var op = $("<option>").text(object[property]);
+    // var op1 = $("<option>").text(object[property]);
     op.val(object[property]);
-    $("#select").append(response.op);
-    $("#select2").append(response.op);
+    $("#select").append(op.clone());
+    $("#select2").append(op.clone());
     // $("#select2").append(op);
         }
     }
