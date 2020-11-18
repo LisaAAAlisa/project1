ffecbc544ab35d4f557ac273aee04eb42eb973b7
Jin Choo <jinchoo315@gmail.com>
2020-11-18 09:34:14 -0600

    adding to the file

---
+++ script2.js
@@ -0,0 +1,33 @@
+const from_currencyEl = document.getElementById('from_currency');
+const from_ammountEl = document.getElementById('from_ammount');
+const to_currencyEl = document.getElementById('to_currency');
+const to_ammountEl = document.getElementById('to_ammount');
+const rateEl = document.getElementById('rate');
+const exchange = document.getElementById('exchange');
+ 
+from_currencyEl.addEventListener('change', calculate);
+from_ammountEl.addEventListener('input', calculate);
+to_currencyEl.addEventListener('change', calculate);
+to_ammountEl.addEventListener('input', calculate);
+ 
+exchange.addEventListener('click', () => {
+	const temp = from_currencyEl.value;
+	from_currencyEl.value = to_currencyEl.value;
+	to_currencyEl.value = temp;
+	calculate();
+});
+ 
+function calculate() {
+	const from_currency = from_currencyEl.value;
+	const to_currency = to_currencyEl.value;
+	
+
+$.ajax({
+    url: "http://api.currencylayer.com/list?access_key=7c8ce31c312ebeee8c5489c55a4939fd"
+    .then(res => res.json())
+		.then(res => {
+		const rate = res.rates[to_currency];
+		rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`
+		to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
+	})
+  
