cd981d7eb62ddb7c86d20dc56ac1192794894a91
Jin Choo <jinchoo315@gmail.com>
2020-11-19 11:20:27 -0600

    I pulled from the main, other save point, I am to push to the main

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+//setting up ajax request to get a list of currencies from API
 $.ajax({
     url: "http://api.currencylayer.com/list?access_key=7c8ce31c312ebeee8c5489c55a4939fd"
 }).then(function (from) {
     console.log(from)
-    // $("#convertedFrom").text(from.value);
-    // $("#convertedTo").text(from.value)
     searchCurrencyURL(from);
-    //  var from = $("#convertedFrom").text(response);
-    // var to = $("#convertedTo").text(respons);
-    // var amount = $("#search-exchange").text();
-    // var res = $("#response").text();
 })
+
+//function to get the currency list from API do display in drop down menus
 function searchCurrencyURL(response) {
+
     const object = response.currencies;
+
     for (const property in object) {
         console.log(`${property}: ${object[property]}`);
         //this object[property] is the long form name of our currency
@@ -30,27 +29,33 @@ function getExchange() {
     var from = $("#convertedFrom").val();
     var to = $("#convertedTo").val();
     console.log(amount, from, to)
+
     var convert = "http://api.currencylayer.com/convert?access_key=7c8ce31c312ebeee8c5489c55a4939fd&from=" + from + "&to=" + to + "&amount=" + amount + "&format=1"
+//setting up ajax call to get a response for the currency conccecrsions
     $.ajax({
         url: convert,
         method: "GET"
     }).then(function (response) {
-        // $("#response")
         console.log(response)
+        //this sets up the response to be displayed to user
         $("#output").val(response.result.toFixed(2));
         console.log(response.result);
     })
+
 }
-//event handlers for drop down and user inmput
+//event handlers for drop down 
 $("#convertedFrom").on("change", function () {
     getExchange();
 });
 $("#convertedTo").on("change", function () {
     getExchange();
 });
+//event handler for user input
 $("#search-exchange").on("input", function () {
     getExchange();
 });
+//date functions to display date on top of the constant currencies display
+//first formats the date
 function formatDate() {
     var now = new Date();
     var day = ("0" + now.getDate()).slice(-2);
@@ -58,20 +63,26 @@ function formatDate() {
     var formattedDate = (month) + "/" + (day) + "/" + now.getFullYear();
     return (formattedDate);
 }
+//second functino displayes the date
 function getCurrentDay() {
     var day = formatDate();
     let currentDay = document.querySelector('.current-date');
-    currentDay.innerHTML = "<strong>Date: " + day + "</strong>";
+    currentDay.innerHTML = "<strong>DATE: " + day + "</strong>";
     console.log(currentDay);
 }
 getCurrentDay();
+
+//setting up variables for the constant currencies display
 var AUD = $(".AUD");
 var USD = $(".USD");
 var CAD = $(".CAD");
+
 queryUrl = "https://api.exchangeratesapi.io/latest?symbols=USD,AUD,CAD";
+//ajax call to get the current exchange rate with Euro as base
 $.ajax({
     url: queryUrl,
     method: "GET"
+
 }).then(function (response) {
     console.log(response);
     var currencies = response;
@@ -79,19 +90,3 @@ $.ajax({
     USD.html("USD: " + "<i class='fa fa-dollar'></i>" + currencies.rates.USD.toFixed(2));
     CAD.html("CAD: " + "<i class='fa fa-dollar'></i>" + currencies.rates.CAD.toFixed(2));
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
